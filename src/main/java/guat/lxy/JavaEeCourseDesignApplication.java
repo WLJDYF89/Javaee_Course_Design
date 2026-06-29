abf6727a9142caafc82490d98f7fcda6f9eb73cb
wljdyf89 <qianzhandeng555@qq.com>
2026-06-29 20:43:27 +0800

    首次提交-测试

---
+++ src/main/java/guat/lxy/JavaEeCourseDesignApplication.java
@@ -0,0 +1,13 @@
+package guat.lxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaEeCourseDesignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaEeCourseDesignApplication.class, args);
+    }
+
+}
